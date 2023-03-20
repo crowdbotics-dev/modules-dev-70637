3c2905a13ffe2c366d83d68eebc282e8eb0305a6
Mohit Upadhyay <mohit.upadhyay@crowdbotics.com>
2023-03-20 13:27:17 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "modules-dev-70637",
-  url: "https://modules-dev-70637.botics.co",
-  api: "https://modules-dev-70637.botics.co/api/v1"
-}
+  "name": "modules-dev-70637",
+  "url": "https://modules-dev-70637.botics.co",
+  "api": "https://modules-dev-70637.botics.co/api/v1",
+  "initialRoute": "Camera"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
